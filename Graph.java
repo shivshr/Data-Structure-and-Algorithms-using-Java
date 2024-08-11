dfa1171f1c0f8f7440dc86b3f235acd62a1250bb
shivshr <106402908+shivshr@users.noreply.github.com>
2024-08-11 20:27:18 +0530

    Create Graph.java

---
+++ Graph.java
@@ -0,0 +1,51 @@
+import java.util.LinkedList;
+
+class Graph {
+    private int V;
+    private LinkedList<Integer>[] adj;
+
+    Graph(int V) {
+        this.V = V;
+        adj = new LinkedList[V];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void BFS(int s) {
+        boolean[] visited = new boolean[V];
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        visited[s] = true;
+        queue.add(s);
+
+        while (queue.size() != 0) {
+            s = queue.poll();
+            System.out.print(s + " ");
+
+            for (int n : adj[s]) {
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        System.out.println("Breadth First Traversal starting from vertex 2:");
+        g.BFS(2); // Output: 2 0 3 1
+    }
+}
